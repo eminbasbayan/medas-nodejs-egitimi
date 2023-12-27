9ab1fb363656bd73563cc2bb87cbdb64a7f100f9
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-27 11:10:51 +0300

    module scope

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const add = require("./add.js");
-const addFunction = require("./add.js");
-
-const sum = add(4, 5);
-const sum2 = addFunction(10, 25);
-console.log(sum);
-console.log(sum2);
+require("./batman.js");
+require("./superman.js");
