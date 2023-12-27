9e7c0607e8a2b6850ebea30b121f0dd00353d1c4
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-27 13:49:56 +0300

    module caching

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-require("./batman.js");
-require("./superman.js");
+/* const superHero = require("./super-hero.js");
 
-// (function(exports, require, module, __filename, __dirname){
+console.log(superHero.getName()); // Batman
 
-// })()
+superHero.setName("Superman");
+console.log(superHero.getName()); // Superman
+
+const newSuperHero = require("./super-hero.js");
+console.log(newSuperHero.getName()); // Superman
+ */
+const SuperHero = require("./super-hero.js");
+const superman = new SuperHero("Superman");
+const batman = new SuperHero("Batman");
+console.log(superman);
+console.log(batman);
