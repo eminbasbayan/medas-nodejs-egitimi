b2eb08ffd28dc612c4b94cb05f77cc41382b32de
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-27 10:58:22 +0300

    module exports

--- add.js
+++ add.js
@@ -1,5 +1,5 @@
 const add = (a, b) => {
   return a + b;
 };
-const sum = add(1, 5);
-console.log(sum);
+
+module.exports = add;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require("./add.js");
+const add = require("./add.js");
+const addFunction = require("./add.js");
+
+const sum = add(4, 5);
+const sum2 = addFunction(10, 25);
+console.log(sum);
+console.log(sum2);
