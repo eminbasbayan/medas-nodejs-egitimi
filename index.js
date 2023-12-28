9b956b86a1f3125224796bcf917d50e0ba1493ed
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-28 15:21:32 +0300

    http routing

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const http = require("node:http");
-const path = require("node:path");
-const fs = require("node:fs");
 
-const server = http.createServer((request, response) => {
-  const name = "Emin Başbayan";
-  const filePath = path.join(__dirname, "index.html");
-  fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
-    if (err) {
-      response.writeHead(500);
-      response.end("Error loading the file!");
-    } else {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end(data.replace("{{name}}", name));
-    }
-  });
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home Page");
+  } else if (req.url === "/products") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Products Page");
+  } else if (req.url === "/api/customers") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify([
+        {
+          firstName: "Emin",
+          lastName: "Başbayan",
+        },
+        {
+          firstName: "Emine",
+          lastName: "Başbayan",
+        },
+      ])
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not found!");
+  }
 });
 
 const PORT = 5000;
