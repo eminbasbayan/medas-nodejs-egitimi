a3eed7dd9a3a70d7285026bf700888f5efe9949f
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-28 10:18:00 +0300

    drink machine örneği

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const PizzaShop = require("./pizza-shop.js");
+const DrinkMachine = require("./drink-machine.js");
 const pizzaShop = new PizzaShop();
+const drinkMachine = new DrinkMachine();
 
 pizzaShop.on("order", (size, topping) => {
   console.log(`Sipariş alındı: ${size} pizza, ${topping} ile.`);
+  drinkMachine.serveDrink(size);
 });
 
 pizzaShop.order("large", "zeytin");
