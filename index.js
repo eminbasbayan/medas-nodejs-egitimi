3944275844c3235341b1ce0ff1b342f3dd920d8b
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-28 13:28:36 +0300

    fs promise module

--- index.js
+++ index.js
@@ -1,61 +1,19 @@
-const fs = require("node:fs");
+// const fs = require("node:fs/promises");
+const fs = require("node:fs").promises;
 
-//! Senkron Dosya yoksa oluşturma varsa içine yazma.
-// fs.writeFileSync("./file.txt", "Hello World!!!!");
-//! Asenkron Dosya yoksa oluşturma varsa içine yazma.
-// fs.writeFile("./file.txt", "Emin Başbayan", (err) => {
-//   if (err) {
-//     console.log("Dosya yazarken bir hata oluştu:", err);
-//   } else {
-//     console.log("Dosya başarıyla yazıldı!");
+// async function readFile() {
+//   try {
+//     const data = await fs.readFile("./file.txt", "utf-8");
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
 //   }
-// });
+// }
 
-//! Dosya üzerine yazı ekleme
-// fs.writeFile("./file.txt", "Emin Başbayan", { flag: "a" }, (err) => {
-//   if (err) {
-//     console.log("Dosya yazarken bir hata oluştu:", err);
-//   } else {
-//     console.log("Dosya başarıyla yazıldı!");
-//   }
-// });
+// readFile();
 
-//! Senkron dosya okuma
-// const txtFile = fs.readFileSync("./file.txt","utf-8");
-//! Asenkron dosya okuma
-// fs.readFile("./file.txt", (err, data) => {
-//   setTimeout(() => {
-//     if (err) {
-//       console.log("Dosya okunurken bir hata oluştu:", err);
-//     } else {
-//       console.log(data.toString());
-//     }
-//   }, 3000);
-// });
-// console.log("async");
-
-//! Dosya silme
-// fs.unlink("./file.txt", (err) => {
-//   if (err) {
-//     console.log("Dosya silinemedi:", err);
-//   } else {
-//     console.log("Dosya başarıyla silindi!");
-//   }
-// });
-
-//! Klasör oluşturma
-// fs.mkdir("./images", (err) => {
-//   if (err) {
-//     console.log("Klasör oluşturulamadı:", err);
-//   } else {
-//     console.log("Klasör başarıyla oluştu!");
-//   }
-// });
-//! Klasör silme
-fs.rmdir("./images", (err) => {
-  if (err) {
-    console.log("Klasör silinmedi:", err);
-  } else {
-    console.log("Klasör başarıyla silindi!");
-  }
-});
+fs.readFile("./file.txt", "utf-8")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
