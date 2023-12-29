3327acab364f67d80e7ded3d42156c556d5c3658
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-29 09:27:52 +0300

    express regex kullanımı

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
-app.get("/", (req, res) => {
+app.get("^/$|index(.html)?", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get("/new-page.html", (req, res) => {
+app.get("^/$|new-page(.html)?", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-app.get("/old-page", (req, res) => {
+app.get("^/$|old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html");
 });
 
