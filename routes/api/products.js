97c2f872535993a725625b7c07f1fc6059576ac5
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-29 16:33:44 +0300

    route params

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,8 +3,35 @@ const router = express.Router();
 const data = {};
 data.products = require("../../data/products.json");
 
-router.route("/").get((req, res) => {
-  res.json(data.products);
-})
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(data.products);
+  })
+  .post((req, res) => {
+    res.json({
+      productName: req.body.productName,
+      categoryName: req.body.categoryName,
+    });
+  })
+  .put((req, res) => {
+    res.json({
+      productName: req.body.productName,
+      categoryName: req.body.categoryName,
+    });
+  })
+  .delete((req, res) => {
+    res.json({
+      id: req.body.id,
+    });
+  });
+
+router.route("/:productId").get((req, res) => {
+  const productId = req.params.productId;
+  const findProduct = data.products.find(
+    (item) => item.id === Number(productId)
+  );
+  res.json(findProduct);
+});
 
 module.exports = router;
