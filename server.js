89a123985f2a32f309c98e7ecd9ddc05260ad98c
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-29 11:33:08 +0300

    cors whiteList

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const whiteList = [
+  "https://www.google.com.tr",
+  "https://bilgisayargenetigi.com",
+  "http://localhost:5000",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Bu adrese izin verilmedi!"));
+    }
+  },
+};
+
 // Cross Origin Resource Sharing (CORS)
-app.use(cors());
+app.use(cors(corsOptions));
 
 // content-type application/x-www-form-urlendcoded
 app.use(express.urlencoded({ extended: false }));
