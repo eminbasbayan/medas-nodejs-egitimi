6b0b92aca6846ecac7a78843b324b867d31eb0a7
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-12-29 15:32:31 +0300

    reqLog middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("node:path");
 const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
+const reqHandler = require("./middleware/reqHandler");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,8 @@ const whiteList = [
   "https://bilgisayargenetigi.com",
   "http://localhost:5000",
 ];
+
+app.use(reqHandler);
 
 const corsOptions = {
   origin: (origin, callback) => {
