c520f95d5905f8d9c74d6d22642ae47b20de89cd
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2024-01-05 11:51:34 +0300

    refresh router server'a eklema

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/route.js"));
 app.use("/register", require("./routes/register.js"));
 app.use("/auth", require("./routes/auth.js"));
+app.use("/refresh", require("./routes/refresh.js"))
 
 app.use(verifyJWT);
 app.use("/products", require("./routes/api/products.js"));
