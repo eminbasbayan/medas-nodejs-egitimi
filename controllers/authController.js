f70da3784b22e27ecce639279f8d61b700f0a337
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2024-01-05 09:48:33 +0300

    JWT doğrulama middleware'i oluşturma

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,7 +44,7 @@ const handleLogin = async (req, res) => {
 
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
-      maxAge: 24 * 24 * 60 * 1000, // one day
+      maxAge: 24 * 60 * 60 * 1000, // one day
     });
 
     return res.status(200).json({
