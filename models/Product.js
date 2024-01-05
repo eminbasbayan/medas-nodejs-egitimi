6fdd90a9e250f5547db6db038900976a43a09f1c
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2024-01-05 15:37:03 +0300

    product schema oluşturuldu

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+});
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
